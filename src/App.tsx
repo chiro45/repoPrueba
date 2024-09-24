14c95d53e4b28047bd189c5b440a3c49d3fc22cf
chiro45 <luciano.chiroli45@gmail.com>
2024-09-24 09:10:04 -0300

    contador terminado

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,27 @@
+import { useState } from "react";
+
 function App() {
-  return <div></div>;
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setCount((prev) => prev + 1);
+        }}
+      >
+        Incrementar
+      </button>
+      <p>{count}</p>
+      <button
+        onClick={() => {
+          setCount((prev) => prev - 1);
+        }}
+      >
+        Decrementar
+      </button>
+    </div>
+  );
 }
 
 export default App;
